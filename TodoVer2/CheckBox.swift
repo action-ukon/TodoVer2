7840fd210b2d90e8c4c822ff1ff558f2c2346693
action-ukon <actionukon0614mhf@gmail.com>
2022-01-15 19:32:13 +0900

    checkbox to init

--- TodoVer2/CheckBox.swift
+++ TodoVer2/CheckBox.swift
@@ -7,17 +7,42 @@
 
 import SwiftUI
 
-struct CheckBox: View {
-    @State var checked: Bool = false
+struct CheckBox<Label>: View where Label: View{
+    // @Binding 親の値と共有したい
+    @Binding var checked: Bool
+    
+    private var label: ()-> Label
+    
+    public init(checked: Binding<Bool>,
+                @ViewBuilder label: @escaping ()-> Label)
+    {
+        self._checked = checked
+        self.label = label
+    }
+    
     var body: some View {
-        Toggle(isOn: $checked) {
-            Text("チェックボックス")
+        // チェックマーク切り替え
+        HStack {
+            Image(systemName: checked ? "checkmark.circle": "circle")
+                .onTapGesture {
+                    self.checked.toggle()
+                }
+            label()
         }
     }
 }
 
 struct CheckBox_Previews: PreviewProvider {
     static var previews: some View {
-        CheckBox()
+        VStack {
+            CheckBox(checked:
+                            .constant(false)){
+                Text("牛乳を買う")
+            }
+            CheckBox(checked:
+                            .constant(true)){
+                Text("牛乳を買う")
+            }
+        }
     }
 }
