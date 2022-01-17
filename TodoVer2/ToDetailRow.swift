571e74e69afca3094c1f0a3ecdc5c51fe80e2b8c
action-ukon <actionukon0614mhf@gmail.com>
2022-01-18 01:21:44 +0900

    make gesture

--- TodoVer2/ToDetailRow.swift
+++ TodoVer2/ToDetailRow.swift
@@ -12,9 +12,12 @@ struct TodoDetailRow: View {
     // @State ひとつの変数だけを監視し、変更があれば画面に反映
     // @ObservedObject 変数が持つメンバに変更があれば画面に反映
     @ObservedObject var todo: TodoEntity
+    var hideIcon = false
     var body: some View {
         HStack {
-            CategoryImage(TodoEntity.Category(rawValue: todo.category))
+            if !hideIcon {
+                CategoryImage(TodoEntity.Category(rawValue: todo.category))
+            }
             // TodoEntityの以下のrawValue Binding<Bool>
             // enum State: Int16 {
                 //case todo
@@ -32,19 +35,46 @@ struct TodoDetailRow: View {
                 }
             }.foregroundColor(self.todo.state == TodoEntity.State.done.rawValue
                               ? .secondary : .primary)
-        }
+        // スライドすると取り消し線とチェック　逆も
+        }.gesture(DragGesture().onChanged({ value in
+            if value.predictedEndTranslation.width > 200 {
+                if self.todo.state != TodoEntity.State.done.rawValue {
+                    self.todo.state = TodoEntity.State.done.rawValue
+                }
+            }else if value.predictedEndTranslation.width < -200 {
+                if self.todo.state != TodoEntity.State.todo.rawValue {
+                    self.todo.state = TodoEntity.State.todo.rawValue
+                }
+            }
+        }))
     }
 }
 
 struct TodoDetailRow_Previews: PreviewProvider {
     static var previews: some View {
         let context = PersistenceController.shared.container.viewContext
-        
+
         let newTodo = TodoEntity(context: context)
         newTodo.task = "将来への人間関係づくり"
-        newTodo.state = TodoEntity.State.todo.rawValue
+        newTodo.state = TodoEntity.State.done.rawValue
         newTodo.category = 0
-        
-        return TodoDetailRow(todo: newTodo)
+        let newTodo1 = TodoEntity(context: context)
+        newTodo1.task="クレームへの対応"
+        newTodo.category = 1
+        let newTodo2 = TodoEntity(context: context)
+        newTodo2.task="無意味な接待や付き合い"
+        newTodo2.category = 2
+        let newTodo3 = TodoEntity(context: context)
+        newTodo3.task="長時間、必要以上の息抜き"
+        newTodo3.category = 3
+        return VStack(alignment: .leading) {
+            VStack {
+                TodoDetailRow(todo: newTodo)
+                TodoDetailRow(todo: newTodo, hideIcon: true)
+                TodoDetailRow(todo: newTodo1)
+                TodoDetailRow(todo: newTodo2)
+                TodoDetailRow(todo: newTodo3)
+            }.environment(\.managedObjectContext, context)
+        }
     }
 }
