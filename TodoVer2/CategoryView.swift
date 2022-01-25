14543fd9e1fefca453e261796b1a675b84c51924
action-ukon <actionukon0614mhf@gmail.com>
2022-01-26 02:33:29 +0900

    Task count add

--- TodoVer2/CategoryView.swift
+++ TodoVer2/CategoryView.swift
@@ -46,6 +46,9 @@ struct CategoryView: View {
             .onTapGesture {
                 self.showList = true
             }
+            .onAppear {
+                self.numberOfTasks = TodoEntity.count(in: self.viewContext, category: self.category)
+            }
         }
     }
 }
